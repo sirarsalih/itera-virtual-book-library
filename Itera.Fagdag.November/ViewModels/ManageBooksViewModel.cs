82e9da39f211e8cba2a9ff3a17dd2310739cc40a
sirar87 <sirars@gmail.com>
2014-10-26 20:11:06 +0100

    Initial commit.

---
+++ Itera.Fagdag.November/ViewModels/ManageBooksViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Itera.Fagdag.November.ViewModels
+{
+    public class ManageBooksViewModel
+    {
+        [Required]
+        [Display(Name = "Excel file")]
+        public HttpPostedFileBase ExcelSchemaBase { get; set; }
+    }
+}
